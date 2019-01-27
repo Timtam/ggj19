4d44d5c1c1ac6e79b3903cb1179f218313e3a3e7
Sophie Tauchert <999eagle@999eagle.moe>
2019-01-27 09:05:33 +0100

    Update Map

--- scenes/game/scripts/Map.cs
+++ scenes/game/scripts/Map.cs
@@ -1,11 +1,12 @@
 using Godot;
 using System;
+using System.Linq;
 
 public class Map : Spatial
 {
 	public override void _Ready()
 	{
-		foreach (var child in GetChildren())
+		foreach (var child in GetChildren().Concat(GetNode("terrain").GetChildren()))
 		{
 			if (!(child is MeshInstance mesh)) continue;
 			mesh.CreateTrimeshCollision();
